40de9e82c750c973783a430f8d94d8a0f4d9203c
Bernhard Bablok <bablokb@gmx.de>
2024-03-11 19:09:26 +0100

    added noop test configuration

---
+++ .devcontainer/test-noop/post_create.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# -----------------------------------------------------------------------------
+# post_create.sh: noop postCreateCommand-command
+#
+# Author: Bernhard Bablok
+#
+# -----------------------------------------------------------------------------
+
+echo -e "[post_create.sh] starting postCreateCommand\n"
+echo -e "[post_create.sh] PWD=$PWD\n"
+
+# --- that's it!   ------------------------------------------------------------
+
+echo -e "[post_create.sh] setup complete\n"
