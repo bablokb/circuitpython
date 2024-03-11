9c12cfa67338a727136fd073f301bcce4222edf5
Bernhard Bablok <bablokb@gmx.de>
2024-03-11 19:20:29 +0100

    added show test configuration

---
+++ .devcontainer/test-show/post_create.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# -----------------------------------------------------------------------------
+# post_create.sh: postCreateCommand-command with git command
+#
+# Author: Bernhard Bablok
+#
+# -----------------------------------------------------------------------------
+
+echo -e "[post_create.sh] starting postCreateCommand\n"
+echo -e "[post_create.sh] PWD=$PWD\n"
+
+git --no-pager show --summary
+
+# --- that's it!   ------------------------------------------------------------
+
+echo -e "[post_create.sh] setup complete\n"
