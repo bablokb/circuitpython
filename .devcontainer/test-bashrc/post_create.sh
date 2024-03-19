42e86c4d51e80884e907cea70758cd6c52b87b3d
Bernhard Bablok <bablokb@gmx.de>
2024-03-19 08:02:30 +0100

    cleanout nohup test

--- .devcontainer/test-bashrc/post_create.sh
+++ .devcontainer/test-bashrc/post_create.sh
@@ -9,13 +9,8 @@
 echo -e "[post_create.sh] starting postCreateCommand $0\n"
 echo -e "[post_create.sh] PWD=$PWD\n"
 
-#echo -e \
-#  "\n/workspaces/circuitpython/.devcontainer/install_build_env.sh" >> $HOME/.bashrc
-
-nohup /workspaces/circuitpython/.devcontainer/nh_test.sh &
-
-# wait a few seconds to make sure any initial terminals are started
-sleep 10
+echo -e \
+  "\n/workspaces/circuitpython/.devcontainer/install_build_env.sh" >> $HOME/.bashrc
 touch /workspaces/post_create.finished
 
 # --- that's it!   ------------------------------------------------------------
