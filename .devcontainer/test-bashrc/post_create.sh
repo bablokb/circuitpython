a01db08e2c21f91ba6b95e4071fee296e978f86f
Bernhard Bablok <bablokb@gmx.de>
2024-03-19 07:57:14 +0100

    testing scripts started with nohup

--- .devcontainer/test-bashrc/post_create.sh
+++ .devcontainer/test-bashrc/post_create.sh
@@ -9,8 +9,10 @@
 echo -e "[post_create.sh] starting postCreateCommand $0\n"
 echo -e "[post_create.sh] PWD=$PWD\n"
 
-echo -e \
-  "\n/workspaces/circuitpython/.devcontainer/install_build_env.sh" >> $HOME/.bashrc
+#echo -e \
+#  "\n/workspaces/circuitpython/.devcontainer/install_build_env.sh" >> $HOME/.bashrc
+
+nohup /workspaces/circuitpython/.devcontainer/nh_test.sh &
 
 # wait a few seconds to make sure any initial terminals are started
 sleep 10
