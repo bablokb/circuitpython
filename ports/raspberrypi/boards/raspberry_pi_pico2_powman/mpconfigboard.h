1213ac58afaa826dde8643210ea1822f2503a68e
Bernhard Bablok <bablokb@gmx.de>
2025-08-04 06:33:55 +0200

    added board raspberry_pi_pico2_powman

---
+++ ports/raspberrypi/boards/raspberry_pi_pico2_powman/mpconfigboard.h
@@ -0,0 +1,16 @@
+// This file is part of the CircuitPython project: https://circuitpython.org
+//
+// SPDX-FileCopyrightText: Copyright (c) 2024 Scott Shawcroft for Adafruit Industries
+//
+// SPDX-License-Identifier: MIT
+
+#define MICROPY_HW_BOARD_NAME "Raspberry Pi Pico 2"
+#define MICROPY_HW_MCU_NAME "rp2350a"
+
+#define MICROPY_HW_LED_STATUS (&pin_GPIO25)
+
+#define CIRCUITPY_BOARD_I2C         (1)
+#define CIRCUITPY_BOARD_I2C_PIN     {{.scl = &pin_GPIO5, .sda = &pin_GPIO4}}
+
+#define CIRCUITPY_CONSOLE_UART_RX (&pin_GPIO1)
+#define CIRCUITPY_CONSOLE_UART_TX (&pin_GPIO0)
